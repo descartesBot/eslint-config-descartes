96a2bd29ebef7a73f88c1902503b679478f290b8
atshushi <atsushithesushi@gmail.com>
2022-06-06 16:21:07 -0300

    Refactor

--- typescript.js
+++ typescript.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  extends: ['./src/index.js'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-shadow': 'error',
@@ -13,5 +14,6 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-use-before-define': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
   },
 };
